09f619186fe4c17a840ecab24caf180a4a96cc51
yan <xyzy10@gmail.com>
2020-03-22 22:59:55 -0700

    books way

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,22 +1,11 @@
-import { Directive, Input, OnInit, HostListener, ElementRef, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective implements OnInit {
-  @Input() expand:string = '';
-  toggleClass: string = 'open';
-  
-  @HostBinding('class') cssClass: string = this.toggleClass; // CSS Class output.
-
-  @HostListener('click') click (eventData: Event) {
-    this.cssClass = (this.cssClass.length) ? '' : this.toggleClass; // Toggle Class
-  }
-
-  constructor(private eleRef: ElementRef) { }
-
-  ngOnInit() {
-    // Setup default behavior with input state.
-    this.cssClass = Boolean(this.expand) ? this.toggleClass : '';
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen:boolean = false;
+  @HostListener('click') toggleOpen(eventData: Event) {
+    this.isOpen = !this.isOpen;
   }
 }
